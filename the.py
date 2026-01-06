020c958fe71ed96e4d74a922b8cf2ea52d7d978b
ICaPiCo <icapico.ma@gmail.com>
2026-01-06 08:16:57 +0100

    Update the.py

--- the.py
+++ the.py
@@ -2,5 +2,10 @@
 Texas Hold'Em
 
 Rules:
-
+- Every player gets two hole cards.
+- Three communal cards are dealt ('the flop')
+- One more communal card is dealt ('the turn')
+- A final communal card is dealt ('the river')
+- All remaining players reveal their best five-card hand, using all seven available cards.
+- Possible to call (don't add more money) / fold (someone raised) / raise (add more money to the pot) / all in (put all your money in the pot)
 """
