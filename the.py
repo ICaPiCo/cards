ba18a0347d0227aa4bc2648cf9a591a5e3bb4579
ICaPiCo <icapico.ma@gmail.com>
2026-01-06 08:26:51 +0100

    Update the.py

--- the.py
+++ the.py
@@ -10,14 +10,19 @@ Rules:
 - Possible to call (don't add more money) / fold (someone raised) / raise (add more money to the pot) / all in (put all your money in the pot)
 """
 
+import random
+
 class The:
     def __init__(self):
         self.players = [[100], [],1]  # First part is money left, second part is current cards, third part is if he decided to play
-        self.deck = [k+s for k in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] for s in ["C","S","H","D"]]
+        self.deck = [k+s for k in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] for s in ["♣","♠","♡","♢"]]
+        random.shuffle(self.deck)
         print(self.deck)
         self.match()
 
     def match(self):
-        pass
+        self.flop()
+        self.turn()
+        self.river()
 
 The()
